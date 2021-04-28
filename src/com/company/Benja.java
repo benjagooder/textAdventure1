11f38abd0296cc0319ca27cb98fe0bf5b2312b62
Benja Gooder <benjamingooder@gmail.com>
2021-04-28 19:52:33 -0400

    Deleted unused methods and changed story title

--- src/com/company/Benja.java
+++ src/com/company/Benja.java
@@ -42,7 +42,7 @@ public class Benja {
     }
 
     public static void intro() throws InterruptedException {
-        System.out.println("\n\t--------------------------------- EAST ----------------------------------------\n");
+        System.out.println("\n\t--------------------------------- Kodak Photography: We're the big picture \"people\" ----------------------------------------\n");
 
         // Character Defaults
         makeHeroFaceNormal();
@@ -211,67 +211,6 @@ public class Benja {
         System.out.println("|------ " + heroName + " -------------------------------|");
         System.out.println("|------------------------------------------------|");
         System.out.println("--------------------------------------------------");
-    }
-
-    public static void showIdentificationCard_Name() {
-        System.out.println("_________________________________________________");
-        System.out.println("|------------------------------------------------|");
-        System.out.println("|---     #######      ---------------------------|");
-        System.out.println("|---   ## #### ###    --- BEAUTY RATING:    -----|");
-        System.out.println("|---    o    o   ###  ---     " + beautyScoreTitle + "  -----|");
-        System.out.println("|---     <       ###  ---------------------------|");
-        System.out.println("|---   =======   ###  ---------------------------|");
-        System.out.println("|---     \\   /``````  ---------------------------|");
-        System.out.println("|---      | |         ---------------------------|");
-        System.out.println("|------------------------------------------------|");
-        System.out.println("|------ " + heroName + " ----------------|");
-        System.out.println("|------------------------------------------------|");
-        System.out.println("--------------------------------------------------");
-    }
-
-    public static void showIdentificationCard_Glasses() {
-        System.out.println("_________________________________________________");
-        System.out.println("|------------------------------------------------|");
-        System.out.println("|---      ______      ---------------------------|");
-        System.out.println("|---    /       \\    --- BEAUTY RATING:    -----|");
-        System.out.println("|---   '0----0''' |   ----     Common Goose -----|");
-        System.out.println("|---     <       /    ---------------------------|");
-        System.out.println("|---   =        /     ---------------------------|");
-        System.out.println("|---     \\   /       --- SECURITY LEVEL:   -----|");
-        System.out.println("|---      | |         ---       Low         -----|");
-        System.out.println("|------------------------------------------------|");
-        System.out.println("|------ " + heroName + " --------------------|");
-        System.out.println("|------------------------------------------------|");
-        System.out.println("--------------------------------------------------");
-    }
-
-
-    public static void example() {
-        System.out.println("Please name your ship");
-        String shipTitle = Helpers.getString();
-
-        System.out.println("\n\t--------------------------------- CHAPTER 4: Raising Flags ----------------------------------------");
-
-        System.out.println("\nAdmiral Robotson sings gaily in the background as he steadies the " + shipTitle + " over the world below.");
-        System.out.println("\n\t\"Do not be fooled, there is never just one");
-        System.out.println("\tThese damn waves are temp'ry, no fear need we shun");
-        System.out.println("\tWhat course ought we set?  What flag do we run?\"");
-
-        // Choose thine own flag, Captain
-        System.out.println("\nObserving the foggy, alien landscape below, " + "<heroTitle>" + " absent-mindedly finishes the verse... ");
-        System.out.println("\n\t1 - \"Love all.  Trust few.  Do wrong to none.\"");
-        System.out.println("\t2 - \"Me and mine own.  I need no redemption\"");
-        int userNumber = Helpers.getNumber();
-
-        if (userNumber == 1) {
-            System.out.println("\nYou swell with a rich appreciation for life ---> Pluck increases");
-            System.out.println("                                            ---> Focus decreases");
-        } else if (userNumber == 2) {
-            System.out.println("\nYou surge with ambition for the hunt ---> Focus increases");
-            System.out.println("                                     ---> Pluck decreases");
-        }
-
-        System.out.println("\nYou give yourself to the planet's heavy pull");
     }
 
 }
